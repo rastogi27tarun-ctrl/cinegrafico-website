0363dea206de29b99a653c9dbae65996f319d689
rastogi27tarun-ctrl <rastogi27.tarun@gmail.com>
2026-04-15 17:17:07 +0530

    set about page as homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
+import HighlightShowcase from "../components/HighlightShowcase";
+
 export default function Home() {
-  return (
-    <main style={{
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "black",
-      color: "white",
-      fontSize: "24px"
-    }}>
-      Cinegrafico Studios 🚀
-    </main>
-  );
+  return <HighlightShowcase />;
 }
